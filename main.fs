2aff448aa48bb1488a2fb901e232c139a1a717f8
John Donnellan <john@blueTribu.com>
2019-11-27 09:51:39 +0000

    change formatting

--- main.fs
+++ main.fs
@@ -5,7 +5,9 @@ let interleave = Seq.transpose >> Seq.concat
 let sequences =
   seq{
       yield seq{ yield "1"; yield "2"; yield "3"; yield "4"; yield "5"};
+
       yield seq{ yield "FIRST"};
+      
       yield seq{ yield "aaa"; yield "bbb"; yield "ccc"; yield "ddd"; 
                   yield "eee"; yield "fff"; yield "ggg" }
   }
